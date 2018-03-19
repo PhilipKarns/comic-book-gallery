ce2b1913af1bd7c9e534cfb1a415561f27c24769
Philip Karns <pkarns@deloitte.com>
2018-03-19 13:41:44 -0400

    updated comic book model

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -8,22 +8,40 @@ namespace ComicBookGallery.Models
     public class ComicBook
     {
 
-            //will be used to uniquely identify a comic book
-            public int ID { get; set; }
-            
-            //the title of the series this comic book is part of
-            public string SeriesTitle { get; set; }
-            
-            //comic book's issue number
-            public int IssueNumber { get; set; }
+        //will be used to uniquely identify a comic book
+        public int ID { get; set; }
 
-            //comic book's description
-            public int DescriptionHtml { get; set; }
+        //the title of the series this comic book is part of
+        public string SeriesTitle { get; set; }
 
-            //string array that lists the artist who created the comic book
-            public Artist[] Artists { get; set; }
+        //comic book's issue number
+        public int IssueNumber { get; set; }
 
-            //indicates whether this comic book is a favorite of ours or not
-            public bool Favorite { get; set; }
+        //comic book's description
+        public int DescriptionHtml { get; set; }
+
+        //string array that lists the artist who created the comic book
+        public Artist[] Artists { get; set; }
+
+        //indicates whether this comic book is a favorite of ours or not
+        public bool Favorite { get; set; }
+
+        //string that uniquely identifies a comic book for our users
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " # " + IssueNumber;
+            }
+        }
+            //series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-")
+                    .ToLower() + "-" + IssueNumber + ".jpg";                    
+            }
+        }
     }
 }
