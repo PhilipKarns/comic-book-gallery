30c60f11f9a4bda5ce2b7e6f995f251886c30700
Philip Karns <pkarns@deloitte.com>
2018-04-25 14:32:37 -0400

    Updated the default route

--- src/ComicBookGallery/App_Start/RouteConfig.cs
+++ src/ComicBookGallery/App_Start/RouteConfig.cs
@@ -11,12 +11,13 @@ namespace ComicBookGallery
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            //this line of code tells router to ignore all files with .axd extension. These files need to be handled by asp.net and never routed to controller
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            //this is the definition for our default route, which has the route name, the URL pattern that the route should match, and the default values for the route's URL Parameters
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ComicBooks", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
